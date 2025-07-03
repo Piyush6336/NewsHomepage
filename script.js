433b1be40ad7a76bdb3729da94a61e6747a8ab65
Piyush arya <piyush.arya@letzcreate.in>
2025-07-03 23:05:07 +0530

    NewsHomepage

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Get elements
-const menuButton = document.getElementById('menu-button');
-const nav = document.querySelector('nav');
-const closeButton = document.getElementById('menu-close');
+// // Get elements
+// const menuButton = document.getElementById('menu-button');
+// const nav = document.querySelector('nav');
+// const closeButton = document.getElementById('menu-close');
 
-// Open menu (sidebar)
-menuButton.addEventListener('click', () => {
-  nav.classList.add('open');
-});
+// // Open menu (sidebar)
+// menuButton.addEventListener('click', () => {
+//   nav.classList.add('open');
+// });
 
-// Close menu (sidebar)
-closeButton.addEventListener('click', () => {
-  nav.classList.remove('open');
-});
+// // Close menu (sidebar)
+// closeButton.addEventListener('click', () => {
+//   nav.classList.remove('open');
+// });
